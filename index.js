1c045ae12431d65803aebc05dcde6b67d354b647
Matthew Ty <matthewty1220@gmail.com>
2021-12-22 12:54:56 -0700

    leetcode easy question two sum solved with explanation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var twoSum = function(nums, target) {
     // for loop needed to run through the input of the integer array
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
             // another for loop needed to account for the second num inputted
             if (nums[i] + nums[j] === target) {
                 // if integeres inputted = target, return the two nums
