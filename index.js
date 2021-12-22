b1efc13d44aa315973eab4efabddc60177a71b6b
Matthew Ty <matthewty1220@gmail.com>
2021-12-22 12:49:16 -0700

    leetcode easy question two sum solved with explanation

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Two Sum Problem
-
 var twoSum = function(nums, target) {
-    let arrayOfNums = [];
-    for (let i = 0; i < nums.length; i++) 
-        for (let j = 0; i < nums.length; i++) {
+    // for loop needed to run through the input of the integer array
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; i++) {
+            // another for loop needed to account for the second num inputted
             if (nums[i] + nums[j] === target) {
+                // if integeres inputted = target, return the two nums
                 return [i, j]
             }
         }
-};
+    }
+ };
